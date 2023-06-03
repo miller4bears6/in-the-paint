4bc12f809415028d920e884f02bba48707123407
miller4bears6 <102344074+miller4bears6@users.noreply.github.com>
2023-06-03 15:52:51 -0400

    A php program with CRUD operations.
    that uses MySQL to update a localhost database in myphpadmin.
    
    A stock database. -> symbol, name, and price
    Add a stock, delete a stock, view stocks, and delete a stock.
    
    #html #php #MySQL #css

---
+++ php-project-1/index.php
@@ -0,0 +1,146 @@
+<!--
+Fatima Mohammed
+CIS2454 SU-23
+Project 1
+
+https://github.com/Oakland-Community-College/cis2454-summer2023-project1-miller4bears6
+
+-->
+
+<?php
+
+    //connect to db with an external php file
+    require_once 'scripts/database.php';
+    
+    //CRUD OPERATIONS
+    //read-select
+
+    //declare variables here
+    
+    //calculations
+    //this is in the php below
+        
+    //format the vars
+    
+    //more validation
+    
+    //RUN A QUERY HERE - SELECT
+
+    //next set up a loop in the body of the html to display data
+
+
+    
+    
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Fatima's Project 1</title>
+        <link rel="stylesheet" href="scripts/style.css">
+    </head>
+    <body>
+        <h1>Welcome to Fatima's Project 1</h1>
+        
+        <h2>Stock Application with a Database</h2>
+        
+        <p id="queryError"><?php echo $queryError; ?></p>
+
+        
+        <!--select operation-->
+        
+        <p>SELECT from Stocks Table:</p>
+        
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Symbol</th>
+                <th>Price</th>
+                <th>ID</th>
+            </tr>
+
+            <?php foreach ($stocks as $stock) : ?>
+
+            <tr>
+                <td><?php echo $stock['symbol']; ?></td> 
+                <td><?php echo $stock['name']; ?></td>
+                <td><?php echo '$'.$stock['price']; ?></td>
+                <td><?php echo $stock['id']; ?></td>
+            </tr>
+
+            <?php endforeach; ?> 
+        </table><br>
+        <hr>
+        
+        <!--insert operation-->
+        <!--add a form to input data-->
+        <p>INSERT to Stocks Table:</p>
+
+        <form id="insert_form" method="post" action ="index.php">
+              
+            <div id="userForm1">
+                <label>Symbol: </label>
+                <input type="text" name="symbol" size="10" /><br><br>             
+                <label>Name: </label>
+                <input type="text" name="name" size="30" /><br><br>            
+                <label>Market Price: </label>
+                $ <input type="text" name="price" size="10" /><br><br>
+                <input type="hidden" name='action' value='insert' />
+            </div>
+            <div id="button">
+                <input id="btn_add_stock" type="submit" name="submit" value="ADD A STOCK"/> <br><br>
+            </div>
+            
+        </form>
+        
+        <hr>
+        
+        <p>UPDATE Stocks Table:</p>
+
+        <form id="update_form" method="post" action ="index.php">
+              
+            <div id="userForm2">
+                <label>Symbol: </label>
+                <input type="text" name="symbol" size="10" /><br><br>             
+                <label>Name: </label>
+                <input type="text" name="name" size="30" /><br><br>            
+                <label>Market Price: </label>
+                $ <input type="text" name="price" size="10" /><br><br>
+                <input type="hidden" name='action' value='update' />
+            </div>
+            <div id="button2">
+                <input id="btn_upd_stock" type="submit"  value="UPDATE A STOCK"/> <br><br>
+            </div>
+            
+        </form>
+        
+        <hr>
+        
+        <p>DELETE From The Stocks Table:</p>
+
+        <form id="delete_form" method="post" action ="index.php">
+              
+            <div id="userForm3">
+                <label>Symbol: </label>
+                <input type="text" name="symbol" size="10" required/><br><br>
+                <input type="hidden" name='action' value='delete' />
+            </div>
+            <div id="button3">
+                <input id="btn_del_stock" type="submit" name="submit" value="DELETE A STOCK"/> <br><br>
+            </div>
+            
+        </form>
+        
+        
+            
+        <?php ?>
+        <br><br>
+        
+        <div id = "results">
+            
+        </div>
+        
+    </body>
+</html>
---
+++ php-project-1/scripts/database.php
@@ -0,0 +1,155 @@
+<?php
+
+
+//configuration of the data source name
+//mysql:host=host_address;dbname=database_name;
+
+    //set up connection variables to connect to the stock db
+
+    //create PDO object with the variables defined ^
+    $dsn = 'mysql:host=localhost; dbname=stock';
+    $username = 'testUser';
+    $password = 'test';
+    $action = htmlspecialchars(filter_input(INPUT_POST, "action"));
+
+    
+    //try-catch to test the connection
+    //run queries here
+    try {
+        //connect to the database
+        $stockDB = new PDO($dsn, $username, $password);
+        echo '<p>Database Connection Status: CONNECTED! :) </p>';
+        
+        //get the values from the form and check format
+        $symbol= htmlspecialchars(filter_input(INPUT_POST,"symbol"));
+        $name = htmlspecialchars(filter_input(INPUT_POST,"name"));
+        $price = filter_input(INPUT_POST,"price",FILTER_VALIDATE_FLOAT);
+        
+        //start INSERT QUERY
+        if ($action == "insert" && $symbol != "" && $name != "" && $price != 0) {
+            
+        //use substitution to insert values via a query
+        //do not directly add content to the fields - sql injection
+            $query = "INSERT INTO stocks (symbol, name, price) VALUES (:symbol,:name,:price)";
+
+            $statement = $stockDB->prepare($query);
+            //sanitize, value bind in PDO to protect against SQL injection
+            $statement->bindValue(':symbol',$symbol);
+            $statement->bindValue(':price',$price);
+            $statement->bindValue(':name',$name);
+            //execute query
+            $statement->execute();
+            //close cursor
+            $statement->closeCursor();
+
+        }//end of insert query
+        
+        //start UPDATE query
+        else if ($action == "update" && $symbol != "" && $name != "") {
+
+            //match on a symbol in the update form
+            $query = "UPDATE stocks SET name = :name, price = :price WHERE symbol = :symbol";
+
+            $statement = $stockDB->prepare($query);
+            //sanitize
+            $statement->bindValue(':symbol',$symbol);
+            $statement->bindValue(':price',$price);
+            $statement->bindValue(':name',$name);
+            //execute query
+            $statement->execute();
+            //close cursor
+            $statement->closeCursor();
+
+        }//end of update query        
+         
+        
+        else if ($action == "delete" && $symbol != "") {
+
+            $query2 = "DELETE FROM stocks WHERE symbol = :symbol";
+
+            $statement = $stockDB->prepare($query2);
+            //sanitize
+            $statement->bindValue(':symbol',$symbol);
+
+            //execute query
+            $statement->execute();
+
+            //close cursor
+            $statement->closeCursor();
+
+        }//end of delete record        
+
+        //start of CRUD - READ - SELECT
+        $query = "SELECT name,symbol,price,id FROM stocks";
+        //$stocks = $stockDB->query($query);        
+        
+        $statement = $stockDB->prepare($query);
+        //execute query
+        $statement->execute();
+        $stocks = $statement->fetchAll();
+        //close cursor
+        $statement->closeCursor();
+        //end CRUD - READ - SELECT
+        
+        /*
+        //error check if submit button is pressed and no fields have data
+        if ($action != "") {
+             $queryError = "Can't Insert, Update, or Delete from Table! Missing symbol, name, and price!";
+        }//end of error check
+         * 
+         */
+        
+    }//end of try block
+    
+    catch (Exception $e) {
+        $error_msg = $e->getMessage();
+        echo '<p>NOT CONNECTED! Check Settings :( </p>';
+        echo "<p>Error Msg: $error_msg </p>";
+        exit();
+    }//end of catch block
+
+    
+
+    
+
+    
+    
+
+    
+    
+    
+    
+    //RUN A QUERY TO DELETE A STOCK
+    //look for the input stock from the form
+    
+    //example from the book - works for me
+    $symbol_d= htmlspecialchars(filter_input(INPUT_POST,'symbol'));
+    $name_d = htmlspecialchars(filter_input(INPUT_POST,'name'));
+    //$price = filter_input(INPUT_POST,'price',FILTER_VALIDATE_FLOAT);
+    
+    //if form is filled out and values exist...
+    /*
+    if ($symbol != "" && $name != "") {
+        
+        //use substitution to insert values via a query
+        $query3 = 'DELETE FROM stocks WHERE stocks.symbol = 7';
+        
+        $statement = $stockDB->prepare($query3);
+        //sanitize
+        $statement->bindValue(':symbol',$symbol);
+        $statement->bindValue(':price',$price);
+        $statement->bindValue(':name',$name);
+        
+        //execute query
+        $statement->execute();
+        
+        //close cursor
+        $statement->closeCursor();
+        
+        //$addStock = $stockDB->query($query2);
+
+    }
+     */
+    
+    
+?>
